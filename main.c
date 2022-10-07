0f0f7819efdb9b43223df0b6d982c7b2ff78ccdd
Matthew Finerty <mfinerty@cpp.edu>
2022-10-07 09:57:06 -0700

    Create main.c with outline

---
+++ main.c
@@ -0,0 +1,31 @@
+// Tic Tac Toe game between 2 players or player vs. computer
+#include <stdio.h>
+#include <stdlib.h>>
+
+int main() {
+    // Prompt user for game they wish to play
+
+    // Read information from console
+
+    // Process input
+
+    // Initialize values (array)
+
+    /*
+     * Game loop
+     */
+    // Display board
+
+    // Let player 1 move
+
+    // Check if there's a winner
+
+    // Display board
+
+    // Let player 2 or computer move
+
+    // Check if there's a winner
+
+    // Exit
+    return 0;
+}
