70f8e414b7dedad526a758f1584fb001ed98b69c
Matthew Finerty <mfinerty@cpp.edu>
2022-10-07 10:07:39 -0700

    Print game start info

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 // Tic Tac Toe game between 2 players or player vs. computer
 #include <stdio.h>
-#include <stdlib.h>>
+#include <stdlib.h>
 
 int main() {
     // Prompt user for game they wish to play
+    printf("Welcome to Tic Tac Toe!\n");
+    printf("- 1: Player vs. Player\n");
+    printf("- 2: Player vs. Computer\n");
+    printf("Make a selection (1 or 2): ");
 
     // Read information from console
 
