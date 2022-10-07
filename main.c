5e0f8f702e08102d428219aafca499718000bc0c
Matthew Finerty <mfinerty@cpp.edu>
2022-10-07 12:35:00 -0700

    Implement winner checks

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ void computerMove();
 int checkWinner(int player);
 int checkRow(int row, int player);
 int checkColumn(int column, int player);
-int checkDiagonal(int corner, int player);
+int checkDiagonals(int player);
 
 int main() {
     // Prompt user for game they wish to play
@@ -39,18 +39,33 @@ int main() {
     /*
      * Game loop
      */
-    // Display board
-    printBoard();
+    int winner = 0;
 
-    // Let player 1 move
+    while (winner == 0) {
+        // Display board
+        printBoard();
 
-    // Check if there's a winner
+        // Let player 1 move
 
-    // Display board
+        // Display board
+        printBoard();
 
-    // Let player 2 or computer move
+        // Check if there's a winner
+        checkWinner(1);
 
-    // Check if there's a winner
+        // Let player 2 or computer move
+        if (gameType == pvp) {
+            playerMove(2);
+        } else {
+            computerMove();
+        }
+
+        // Display board
+        printBoard();
+
+        // Check if there's a winner
+        checkWinner(2);
+    }
 
     // Exit
     return 0;
@@ -62,4 +77,27 @@ void printBoard() {
         printf("|%c|%c|%c|\n", board[i][0], board[i][1], board[i][2]);
         printf("-------\n");
     }
+}
+
+int checkRow(int row, int player) {
+    char check = player == 1 ? 'X' : 'O';
+    return board[row][0] == check && board[row][1] == check && board[row][2] == check;
+}
+
+int checkColumn(int column, int player) {
+    char check = player == 1 ? 'X' : 'O';
+    return board[0][column] == check && board[1][column] == check && board[2][column] == check;
+}
+
+int checkDiagonals(int player) {
+    char check = player == 1 ? 'X' : 'O';
+    return (board[0][0] == check && board[1][1] == check && board[2][2] == check ||
+        (board[0][2] == check && board[1][1] == check && board[2][0] == check);
+}
+
+int checkWinner(int player) {
+    int rowWin = checkRow(0, player) || checkRow(1, player) || checkRow(2, player);
+    int columnWin = checkColumn(0, player) || checkColumn(1, player) || checkColumn(2, player);
+    int diagonalWin = checkDiagonals(player);
+    return rowWin || columnWin || diagonalWin ? player : 0;
 }
