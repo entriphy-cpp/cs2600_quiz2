780e7d7d1d8c18692d127f5458d18f9f96e0d312
Matthew Finerty <mfinerty@cpp.edu>
2022-10-07 21:41:00 -0700

    Implement playerMove function

--- main.c
+++ main.c
@@ -98,6 +98,39 @@ void printBoard() {
     }
 }
 
+void playerMove(int player) {
+    printf("PLAYER %d MOVE\n", player);
+
+    int row = 0;
+    while (row < 1 || row > 3) {
+        printf("Enter row: ");
+        scanf("%d", &row);
+        if (row < 1 || row > 3) {
+            printf("Invalid row (must be 1-3)\n");
+        }
+    }
+
+    int column = 0;
+    while (column < 1 || column > 3) {
+        printf("Enter column: ");
+        scanf("%d", &column);
+        if (column < 1 || column > 3) {
+            printf("Invalid column (must be 1-3)\n");
+        }
+    }
+
+    // Decrement values to make it a valid index
+    row--;
+    column--;
+
+    if (board[row][column] == empty) {
+        board[row][column] = player == 1 ? p1 : p2;
+    } else {
+        printf("There is already a mark at this position.\n");
+        playerMove(player);
+    }
+}
+
 int checkRow(int row, int player) {
     enum Mark mark = player == 1 ? p1 : p2;
     return board[row][0] == mark && board[row][1] == mark && board[row][2] == mark;
